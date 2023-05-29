1e2c6ba5c19011ea64af7bafe1ad91bb2642517a
ZahraaAhmd <zahraa.ahmed.ham@gmail.com>
2023-05-29 15:06:33 +0300

    life sec

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -136,7 +136,13 @@ $('.life-slider').slick({
             breakpoint:992,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToScroll: 1,
+            }
+        },{
+            breakpoint:768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
             }
         }
     ]
